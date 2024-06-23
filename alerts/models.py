6847294dfb456cc941f92fefd5bd8853c3f2a3d6
mmarian <mihailmarian12@gmail.com>
2024-06-23 02:01:33 +0100

    user model

--- alerts/models.py
+++ alerts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    hn_username = models.CharField(max_length=100, db_index=True)
+    email = models.EmailField(db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
