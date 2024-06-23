9772a24155ea2c754089f804380eaa6ba819ee96
mmarian <mihailmarian12@gmail.com>
2024-06-23 01:44:18 +0100

    use hn api

---
+++ alerts/hn.py
@@ -0,0 +1,87 @@
+import requests
+from datetime import UTC, datetime, timedelta
+from pydantic import BaseModel
+
+from urllib.parse import urlparse, parse_qs
+
+
+class ItemAuthor(BaseModel):
+    name: str
+    url: str
+
+
+class Item(BaseModel):
+    id: str
+    title: str
+    content_html: str
+    url: str
+    external_url: str
+    date_published: datetime
+    author: ItemAuthor
+
+
+def get_new_comment_replies(
+    username: str, oldest_date_considered: datetime
+) -> list[Item]:
+    replies_url = f"https://hnrss.org/replies.jsonfeed?id={username}"
+    replies_response_json = requests.get(replies_url).json()["items"]
+
+    replies = [Item(**reply) for reply in replies_response_json]
+    filtered_replies = [
+        reply
+        for reply in replies
+        if reply.author.name != username
+        and reply.date_published > oldest_date_considered
+    ]
+
+    return filtered_replies
+
+
+class GetNewPostCommentsResult(BaseModel):
+    user_found: bool
+    items: list[Item]
+
+
+def get_new_post_comments(
+    username: str, oldest_date_considered: datetime
+) -> GetNewPostCommentsResult:
+
+    posts_url = f"https://hnrss.org/submitted.jsonfeed?id={username}"
+    posts_response_json = requests.get(posts_url).json()["items"]
+
+    result = GetNewPostCommentsResult(user_found=False, items=[])
+
+    if posts_response_json == None:
+        return result
+
+    result.user_found = True
+
+    posts = [Item(**post) for post in posts_response_json]
+    oldest_active_post_date = (datetime.now() - timedelta(days=14)).replace(tzinfo=UTC)
+    posts_open_for_discussion = [
+        post for post in posts if post.date_published > oldest_active_post_date
+    ]
+
+    for post in posts_open_for_discussion:
+        parsed_url = urlparse(post.external_url)
+        query_params = parse_qs(parsed_url.query)
+        post_id = query_params["id"][0]
+
+        comments_url = f"https://hnrss.org/item.jsonfeed?id={post_id}"
+        comments_response_json = requests.get(comments_url).json()["items"]
+
+        if comments_response_json is None:
+            continue
+
+        comments = [Item(**comment) for comment in comments_response_json]
+
+        filtered_comments = [
+            comment
+            for comment in comments
+            if comment.author.name != username
+            and comment.date_published > oldest_date_considered
+        ]
+
+        result.items = result.items + filtered_comments
+
+    return result
--- alerts/urls.py
+++ alerts/urls.py
@@ -1,12 +1,50 @@
 from django.urls import path
 from ninja import NinjaAPI
+from ninja import Schema
+
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta, UTC
+
+from .hn import get_new_comment_replies, get_new_post_comments
 
 api = NinjaAPI()
 
 
-@api.get("/add")
-def add(request, a: int, b: int):
-    return {"result": a + b}
+class RenderedItem(Schema):
+    url: str
+    text: str
+    date: str
+
+
+@api.get("/alerts/users/{username}")
+def get_alerts(request, username: str):
+    timeframe = timedelta(days=1)
+    oldest_date_considered = (datetime.now() - timeframe).replace(tzinfo=UTC)
+
+    result = get_new_post_comments(username, oldest_date_considered)
+    # if not result.user_found:
+    #     raise HTTPException(status_code=404, detail="HN username not found")
+
+    comment_replies = get_new_comment_replies(username, oldest_date_considered)
+
+    return {
+        "post_comments": [
+            RenderedItem(
+                url=item.external_url,
+                text=BeautifulSoup(item.content_html, "html.parser").get_text(),
+                date=item.date_published.strftime("%H:%M %d-%m"),
+            )
+            for item in result.items
+        ],
+        "comment_replies": [
+            RenderedItem(
+                url=item.external_url,
+                text=BeautifulSoup(item.content_html, "html.parser").get_text(),
+                date=item.date_published.strftime("%H:%M %d-%m"),
+            )
+            for item in comment_replies
+        ],
+    }
 
 
 urlpatterns = [
