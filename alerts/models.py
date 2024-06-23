9d2073d5d900a951d1a312c0e5087d753a5f987c
mmarian <mihailmarian12@gmail.com>
2024-06-23 07:24:49 +0100

    usrename should be unique in db for now

--- alerts/models.py
+++ alerts/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class User(models.Model):
-    hn_username = models.CharField(max_length=100, db_index=True)
+    hn_username = models.CharField(max_length=100, db_index=True, unique=True)
     email = models.EmailField(db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
