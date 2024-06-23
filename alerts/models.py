baaedf274b150048301171dca596d685c274fbee
mmarian <mihailmarian12@gmail.com>
2024-06-23 07:38:03 +0100

    better showing in admin

--- alerts/models.py
+++ alerts/models.py
@@ -8,3 +8,6 @@ class User(models.Model):
     email = models.EmailField(db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.hn_username
