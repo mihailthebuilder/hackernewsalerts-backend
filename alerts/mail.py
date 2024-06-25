62f985a72161aa047da62503634b314b8ba9fe72
mmarian <mihailmarian12@gmail.com>
2024-06-25 21:13:28 +0100

    mail + started removing user exist checks

---
+++ alerts/mail.py
@@ -0,0 +1,66 @@
+import boto3
+from botocore.exceptions import ClientError
+
+# Replace sender@example.com with your "From" address.
+# This address must be verified with Amazon SES.
+SENDER = "Hacker News Alerts <alerts@hackernewsalerts.com>"
+
+# Replace recipient@example.com with a "To" address. If your account
+# is still in the sandbox, this address must be verified.
+RECIPIENT = "mihailmarian12@gmail.com"
+
+# If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+AWS_REGION = "eu-west-2"
+
+# The subject line for the email.
+SUBJECT = "Amazon SES Test (SDK for Python)"
+
+# The email body for recipients with non-HTML email clients.
+BODY_TEXT = (
+    "Amazon SES Test (Python)\r\n"
+    "This email was sent with Amazon SES using the "
+    "AWS SDK for Python (Boto)."
+)
+
+# The character encoding for the email.
+CHARSET = "UTF-8"
+
+
+def send_verification_email(user):
+    send_mail()
+
+
+def send_mail():
+
+    # Create a new SES resource and specify a region.
+    client = boto3.client("ses", region_name=AWS_REGION)
+
+    # Try to send the email.
+    try:
+        # Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                "ToAddresses": [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                "Body": {
+                    "Text": {
+                        "Charset": CHARSET,
+                        "Data": BODY_TEXT,
+                    },
+                },
+                "Subject": {
+                    "Charset": CHARSET,
+                    "Data": SUBJECT,
+                },
+            },
+            Source=SENDER,
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        print(e.response["Error"]["Message"])
+    else:
+        print("Email sent! Message ID:"),
+        print(response["MessageId"])
