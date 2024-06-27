84f1174786971719f433d7783e6d38e9fe3c5a3b
Mihail Marian <m.marian@elsevier.com>
2024-06-27 15:18:12 +0100

    WIP

--- socialalerts/settings.py
+++ socialalerts/settings.py
@@ -152,9 +152,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
-STATIC_ROOT = "staticfiles/"
+STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
