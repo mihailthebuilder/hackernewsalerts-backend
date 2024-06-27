4af4a398e79dcdcce70b387d4b5eea7ba961aae3
Mihail Marian <m.marian@elsevier.com>
2024-06-27 15:11:23 +0100

    try with this static

--- socialalerts/settings.py
+++ socialalerts/settings.py
@@ -150,7 +150,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = "staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
