e53fe5b4c71e1f98ca6ce33f4849e8d8da9954ba
mmarian <mihailmarian12@gmail.com>
2024-07-12 12:45:51 +0100

    only error for single user + better error logging

--- alerts/tasks.py
+++ alerts/tasks.py
@@ -1,83 +1,83 @@
 from . import models, hn, mail, utils
-from django.contrib.auth.models import User
+from django.contrib.auth import models as admin_models
+import traceback
 
 from django.utils import timezone
 import logging
 
 
-def send_alerts():
-    try:
-        users = models.User.objects.filter(is_verified=True)
-        for user in users:
-            post_comments = hn.get_new_post_comments(
-                user.hn_username, user.last_checked
-            ).items
-            comment_replies = hn.get_new_comment_replies(
-                user.hn_username, user.last_checked
-            )
-            now = timezone.now()
+def check_for_alerts():
+    users = models.User.objects.filter(is_verified=True)
+    error_count = 0
 
-            len_post_comments = len(post_comments)
-            len_comment_replies = len(comment_replies)
+    for user in users:
+        try:
+            process_user(user)
+        except Exception as e:
+            e.add_note(f"error processing user: {user.hn_username}")
+            logging.error(traceback.format_exc())
+            error_count += 1
 
-            if len_post_comments + len_comment_replies > 0:
-                print(f"Preparing to send email to {user.hn_username}")
-                content = ""
-                subject = ""
-
-                if len_post_comments > 0:
-                    comment_word = "comment" if len_post_comments == 1 else "comments"
-
-                    subject = f"{len_post_comments} {comment_word}"
-
-                    content = (
-                        content
-                        + f"You have {len_post_comments} new {comment_word} to your posts:\n\n"
-                    )
-
-                    for comment in post_comments:
-                        date = utils.format_date(comment.date_published)
-
-                        content = (
-                            content
-                            + f"{date} - {comment.author.name} - {comment.external_url}"
-                        )
-                        content = (
-                            content + utils.html_to_str(comment.content_html) + "\n"
-                        )
-
-                if len_comment_replies > 0:
-                    reply_word = "reply" if len_comment_replies == 1 else "replies"
-
-                    if len_post_comments > 0:
-                        subject += " / "
-
-                    subject += f"{len_comment_replies} {reply_word}"
-
-                    content = (
-                        content
-                        + f"You have {len_comment_replies} new {reply_word} to your comments:\n\n"
-                    )
-
-                    for reply in comment_replies:
-                        date = utils.format_date(reply.date_published)
-
-                        content = (
-                            content
-                            + f"{date} - {reply.author.name} - {reply.external_url}"
-                        )
-                        content = content + utils.html_to_str(reply.content_html) + "\n"
-
-                subject += f" - {utils.format_date(now)}"
-                mail.send_mail(user.email, subject, content)
-
-            user.last_checked = now
-            user.save()
-
-    except Exception as e:
-        content = f"Error in alerts: {e}"
-        logging.error(content)
-
-        admin = User.objects.get(is_superuser=True)
+    if error_count > 0:
+        admin = admin_models.User.objects.get(is_superuser=True)
         subject = "hackernewsalerts.com tasks error"
-        mail.send_mail(admin.email, subject, content)
+        mail.send_mail(admin.email, subject, f"{error_count} errors encountered")
+
+
+def process_user(user: models.User):
+    post_comments = hn.get_new_post_comments(user.hn_username, user.last_checked).items
+    comment_replies = hn.get_new_comment_replies(user.hn_username, user.last_checked)
+    now = timezone.now()
+
+    len_post_comments = len(post_comments)
+    len_comment_replies = len(comment_replies)
+
+    if len_post_comments + len_comment_replies > 0:
+        print(f"Preparing to send email to {user.hn_username}")
+        content = ""
+        subject = ""
+
+        if len_post_comments > 0:
+            comment_word = "comment" if len_post_comments == 1 else "comments"
+
+            subject = f"{len_post_comments} {comment_word}"
+
+            content = (
+                content
+                + f"You have {len_post_comments} new {comment_word} to your posts:\n\n"
+            )
+
+            for comment in post_comments:
+                date = utils.format_date(comment.date_published)
+
+                content = (
+                    content + f"{date} - {comment.author.name} - {comment.external_url}"
+                )
+                content = content + utils.html_to_str(comment.content_html) + "\n"
+
+        if len_comment_replies > 0:
+            reply_word = "reply" if len_comment_replies == 1 else "replies"
+
+            if len_post_comments > 0:
+                subject += " / "
+
+            subject += f"{len_comment_replies} {reply_word}"
+
+            content = (
+                content
+                + f"You have {len_comment_replies} new {reply_word} to your comments:\n\n"
+            )
+
+            for reply in comment_replies:
+                date = utils.format_date(reply.date_published)
+
+                content = (
+                    content + f"{date} - {reply.author.name} - {reply.external_url}"
+                )
+                content = content + utils.html_to_str(reply.content_html) + "\n"
+
+        subject += f" - {utils.format_date(now)}"
+        mail.send_mail(user.email, subject, content)
+
+    user.last_checked = now
+    user.save()
