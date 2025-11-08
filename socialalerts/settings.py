1cbef24272e1c896da1dbdd4c802377484fd5cce
Mihail Marian <mihailthebuilder@gmail.com>
2025-11-08 21:13:32 +0000

    security middleware at the top

--- socialalerts/settings.py
+++ socialalerts/settings.py
@@ -73,9 +73,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
