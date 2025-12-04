76429c18ddb6cfe253ec8a4bb664bc9ca7434203
Mihail Marian <mihailthebuilder@gmail.com>
2025-12-04 20:25:59 +0000

    test task improve

--- alerts/tests_tasks.py
+++ alerts/tests_tasks.py
@@ -1,15 +1,21 @@
 from . import tasks, models
 from django.test import TestCase
 from datetime import timedelta
+import os
 from django.utils.timezone import now
 
 
 class HnGetNewCommentReplies(TestCase):
     def test_user(self):
-        user = models.User(
-            hn_username="",
-            email="test@test.com",
-            is_verified=True,
-            last_checked=now() - timedelta(days=1),
-        )
-        # tasks.process_user(user)
+
+        if os.environ["TEST_RUN_TASK"] == "1":
+            user = models.User(
+                hn_username=os.environ["TEST_HN_USERNAME"],
+                email=os.environ["TEST_USER_EMAIL"],
+                is_verified=True,
+                last_checked=now() - timedelta(days=1),
+            )
+
+            tasks.process_user(user)
+        else:
+            print("Skipping test; set TEST_RUN_TASK=1 to run")
